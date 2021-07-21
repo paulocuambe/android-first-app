f73c39e9f5a756372f26f817be89e52f4ab2101a
paulocuambe <paulo.cuambe@sciafrica.net>
2021-07-21 11:47:17 +0200

    cleanup layout

--- app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
+++ app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
@@ -3,9 +3,6 @@ package com.gosenx.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,19 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.btnApply);
-        button.setOnClickListener(e -> {
-            String firstName = getTextOfElement(R.id.etFirstName);
-            String lastName = getTextOfElement(R.id.etLastName);
-            String birthDate = getTextOfElement(R.id.etBirthDate);
-            String country = getTextOfElement(R.id.etCountry);
-
-            Log.d(this.getLocalClassName(), String.format("%s %s, born on %s, from %s has just registered.", firstName, lastName, birthDate, country));
-        });
-    }
-
-    private String getTextOfElement(int i){
-        return ((EditText) findViewById(i)).getText().toString();
     }
 }
