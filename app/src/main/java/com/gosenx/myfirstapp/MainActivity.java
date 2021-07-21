2a35847516b99edab0fa9a2f0df454c48708d53d
paulocuambe <paulo.cuambe@sciafrica.net>
2021-07-21 12:45:52 +0200

    simple calculator

--- app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
+++ app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.gosenx.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(e -> {
+            int firstNumber = getElementValue(R.id.etFirstNumber);
+            int secondNumber = getElementValue(R.id.etSecondNumber);
+            String result = String.valueOf(firstNumber + secondNumber);
+            TextView tvResult = findViewById(R.id.tvResult);
+            tvResult.setText(result);
+        });
+    }
+
+    private int getElementValue(int i) {
+        return Integer.parseInt(((EditText) findViewById(i)).getText().toString());
     }
 }
