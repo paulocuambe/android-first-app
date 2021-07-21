144dd30add005b797db986b3468264f327b81df9
paulocuambe <paulo.cuambe@sciafrica.net>
2021-07-21 11:42:36 +0200

    update textview dynamically

--- app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
+++ app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
@@ -6,12 +6,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCount = findViewById(R.id.btnCount);
+        TextView tvCount = findViewById(R.id.tvCount);
+
+        btnCount.setOnClickListener(e -> {
+            count++;
+            tvCount.setText(String.format("Let's count together: %s", count));
+        });
+
     }
 }
