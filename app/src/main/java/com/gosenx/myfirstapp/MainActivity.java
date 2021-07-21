9a77a226e51532cec90f0e2d9d7e173cb1fb54a8
paulocuambe <paulo.cuambe@sciafrica.net>
2021-07-21 10:35:55 +0200

    add event listener

--- app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
+++ app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
@@ -3,10 +3,9 @@ package com.gosenx.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.btnApply);
+        button.setOnClickListener(e -> {
+            String firstName = getTextOfElement(R.id.etFirstName);
+            String lastName = getTextOfElement(R.id.etLastName);
+            String birthDate = getTextOfElement(R.id.etBirthDate);
+            String country = getTextOfElement(R.id.etCountry);
+
+            Log.d(this.getLocalClassName(), String.format("%s %s, born on %s, from %s has just registered.", firstName, lastName, birthDate, country));
+        });
+    }
+
+    private String getTextOfElement(int i){
+        return ((EditText) findViewById(i)).getText().toString();
     }
 }
--- app/src/main/java/com/gosenx/myfirstapp/MyWorker.java
+++
@@ -1,7 +0,0 @@
-package com.gosenx.myfirstapp;
-
-public class MyWorker {
-    public static int doubleTheValue(int value) {
-        return 2 * value;
-    }
-}
