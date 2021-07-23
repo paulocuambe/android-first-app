c522336550bf86474ca73ce0c35d064ec91b317c
paulocuambe <paulo.cuambe@sciafrica.net>
2021-07-23 14:45:07 +0200

    choose photo from galery using any galery app

--- app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
+++ app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
@@ -1,8 +1,18 @@
 package com.gosenx.myfirstapp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnTakePhoto = findViewById(R.id.btnTakePhoto);
+
+        btnTakePhoto.setOnClickListener(e -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivityForResult(intent, 0);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && requestCode == 0) {
+            Uri uri = data.getData();
+            if (uri != null) {
+                ImageView ivPhoto = findViewById(R.id.ivPhoto);
+                ivPhoto.setImageURI(uri);
+            }
+        }
     }
 }
