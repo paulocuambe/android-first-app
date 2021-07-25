fe321650dab92676a52153da4d00e48ffcda1ca2
paulocuambe <paulo.cuambe@sciafrica.net>
2021-07-25 16:12:18 +0200

    use registerForActivityResult

--- app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
+++ app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
@@ -1,20 +1,28 @@
 package com.gosenx.myfirstapp;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+    private final ActivityResultLauncher<String> imageResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri result) {
+                    if (result != null) {
+                        ImageView ivPhoto = findViewById(R.id.ivPhoto);
+                        ivPhoto.setImageURI(result);
+                    }
+                }
+            });
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +32,8 @@ public class MainActivity extends AppCompatActivity {
         Button btnTakePhoto = findViewById(R.id.btnTakePhoto);
 
         btnTakePhoto.setOnClickListener(e -> {
-            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.setType("image/*");
-            startActivityForResult(intent, 0);
+            imageResultLauncher.launch("image/*");
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK && requestCode == 0) {
-            Uri uri = data.getData();
-            if (uri != null) {
-                ImageView ivPhoto = findViewById(R.id.ivPhoto);
-                ivPhoto.setImageURI(uri);
-            }
-        }
-    }
 }
