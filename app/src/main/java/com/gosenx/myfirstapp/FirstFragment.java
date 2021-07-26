9999cfafe21c61a7edd4485b8b296bda94f8dd02
paulocuambe <paulo.cuambe@sciafrica.net>
2021-07-26 12:12:20 +0200

    navigate between fragments

---
+++ app/src/main/java/com/gosenx/myfirstapp/FirstFragment.java
@@ -0,0 +1,18 @@
+package com.gosenx.myfirstapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FirstFragment extends Fragment {
+
+    public FirstFragment() {
+        super(R.layout.fragment_first);
+    }
+}
--- app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
+++ app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.gosenx.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btnFirstFragment = findViewById(R.id.btnFragment1);
+        Button btnSecondFragment = findViewById(R.id.btnFragment2);
+
+        FirstFragment firstFragment = new FirstFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.flFragment, firstFragment)
+                .addToBackStack(null)
+                .commit();
+
+        btnFirstFragment.setOnClickListener(e -> {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.flFragment, firstFragment)
+                    .commit();
+        });
+
+        SecondFragment secondFragment = new SecondFragment();
+        btnSecondFragment.setOnClickListener(e -> {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.flFragment, secondFragment)
+                    .addToBackStack(null)
+                    .commit();
+        });
     }
 }
