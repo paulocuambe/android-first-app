84a7aafbe75a9ef27006b0baab8e5d515e6aeb96
paulocuambe <paulo.cuambe@sciafrica.net>
2021-07-26 09:44:20 +0200

    add basic spinner

--- app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
+++ app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.gosenx.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spMonths = findViewById(R.id.spMonths);
+
+        spMonths.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        String.format("You selected %s", adapterView.getItemAtPosition(position).toString()),
+                        Toast.LENGTH_LONG)
+                        .show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
