b7e9bc102c267e362ea8562a7167b860657bd17f
paulocuambe <paulo.cuambe@sciafrica.net>
2021-07-26 09:48:14 +0200

    set spinner values on runtime

--- app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
+++ app/src/main/java/com/gosenx/myfirstapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -16,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner spMonths = findViewById(R.id.spMonths);
+        String[] customList = {"First", "Second", "Third", "Fourth"};
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, customList);
+        spMonths.setAdapter(arrayAdapter);
 
         spMonths.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
